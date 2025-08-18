239a381daa891857d797e3b954b6e1197f4efe45
Mike <80712616+miketorreno@users.noreply.github.com>
2025-08-18 08:30:47 +0300

    Reapply "fix: minor fix"
    This reverts commit 1c04861a12f26b6e5781ab69ac8212aefef21fe0.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -106,6 +106,7 @@ class AuthController extends Controller
             request()->session()->regenerateToken();
         } else {
             if (auth()->user()->currentAccessToken()) {
+                auth()->user()->tokens()->delete();
                 auth()->user()->currentAccessToken()->delete();
                 $response = [
                     'message' => 'Logged out'
@@ -115,9 +116,9 @@ class AuthController extends Controller
             }
 
             $response = [
-                'message' => 'Please login first'
+                'message' => 'Unauthenticated.'
             ];
-            return response($response, 200);
+            return response($response, 401);
         }
     }
 
